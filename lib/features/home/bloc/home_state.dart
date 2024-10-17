b05886ea87f7407b0866ec6fce88404909808c6b
minhtetnaing117 <59909903+minhtetnaing117@users.noreply.github.com>
2024-10-17 13:55:40 +0630

    add home_state of action

--- lib/features/home/bloc/home_state.dart
+++ lib/features/home/bloc/home_state.dart
@@ -3,4 +3,16 @@ part of 'home_bloc.dart';
 @immutable
 sealed class HomeState {}
 
-final class HomeInitial extends HomeState {}
+abstract class HomeActionState extends HomeState {}
+
+class HomeInitial extends HomeState {}
+
+class HomeLoadingState extends HomeState {}
+
+class HomeLoadedSuccessState extends HomeState {}
+
+class HomeErrorState extends HomeState {}
+
+class HomeNavigateToWishlistPageActionState extends HomeActionState {}
+
+class HomeNavigateTCartPageActionState extends HomeActionState {}
